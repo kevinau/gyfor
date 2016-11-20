b51c00fad0d35b808e038bc575eb3a4a563b9d8a
Kevin Holloway <kholloway@geckosoftware.com.au>
2016-11-20 19:01:21 +1030

    Initial commit.

---
+++ org.gyfor.http/src/org/gyfor/http/TripleEResource.java
@@ -0,0 +1,24 @@
+package org.gyfor.http;
+
+import org.gyfor.http.api.Context;
+import org.gyfor.http.api.Resource;
+import org.osgi.service.component.annotations.Component;
+
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+
+@Context("/eee")
+@Resource(path="/static", base="static")
+@Resource(extensions={".css"}, base="static")
+@Component(immediate=true)
+public class TripleEResource implements HttpHandler {
+
+  @Override
+  public void handleRequest(HttpServerExchange exchange) throws Exception {
+    //ResponseCodeHandler.HANDLE_404.handleRequest(exchange);
+    exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+    exchange.getResponseSender().send("/eee handler...");
+  }
+
+}
