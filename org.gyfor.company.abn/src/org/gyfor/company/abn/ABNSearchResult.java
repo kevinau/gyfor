eec20b962e0ae93b2b72653ab48f0c6a47d29ff0
Kevin Holloway <kholloway@geckosoftware.com.au>
2017-01-16 12:01:23 +1030

    Completed visual of searchInput.

---
+++ org.gyfor.company.abn/src/org/gyfor/company/abn/ABNSearchResult.java
@@ -0,0 +1,34 @@
+package org.gyfor.company.abn;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.w3c.dom.Node;
+
+
+public class ABNSearchResult {
+
+  private Node responseNode;
+  
+
+  public ABNSearchResult(Node searchResultsPayload) {
+    this.responseNode = XMLUtils.getNode(searchResultsPayload, "/ABRPayloadSearchResults/response");
+  }
+
+
+  public boolean isException() {
+    return XMLUtils.getNode(responseNode, "exception") != null;
+  }
+
+
+  public String getOrganisationName() throws SecurityException, IllegalArgumentException, ClassNotFoundException,
+      NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+    return XMLUtils.getNodeText(responseNode, "businessEntity/mainName/organisationName");
+  }
+
+
+  public String getExceptionDescription() throws SecurityException, IllegalArgumentException, ClassNotFoundException,
+      NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+    return XMLUtils.getNodeText(responseNode, "exception/exceptionDescription");
+  }
+
+}
